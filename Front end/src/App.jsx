4243cc96278e1d69c0d74fef58e2609c58e726b3
Dubem-Ernest <dumbem@gmail.com>
2025-02-21 01:42:40 +0100

    Updated project files

--- Front end/src/App.jsx
+++ Front end/src/App.jsx
@@ -1,18 +1,22 @@
-import './index.css';
+import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
-import UserLayout from './Components/Layout/UserLayout'
+import UserLayout from "./Components/Layout/UserLayout";
+import Home from "./Pages/Home";
 
 function App() {
-
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<UserLayout />} />
-      
-    </Routes>
-  </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+
+        {/* <Route path="/admin" element={<UserLayout />}>
+          <Route path="index" element={<Home />} />
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- Front end/src/Components/Common/Footer.jsx
+++ Front end/src/Components/Common/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { IoLogoInstagram } from "react-icons/io";
+import { RiTwitterXLine } from "react-icons/ri";
+import { TbBrandMeta } from "react-icons/tb";
 import { Link } from "react-router-dom";
-
+import { FiPhoneCall } from "react-icons/fi";
 const Footer = () => {
   return (
     <footer className="border-t py-12">
@@ -40,22 +43,90 @@ const Footer = () => {
             </li>
             <li>
               <Link to="#" className="hover:text-gray-500 transition-colors">
-                Men's top Wear{" "}
+                Women's top Wear{" "}
               </Link>
             </li>
             <li>
               <Link to="#" className="hover:text-gray-500 transition-colors">
-                Men's top Wear{" "}
+                Men's bottom Wear{" "}
               </Link>
             </li>
             <li>
               {" "}
               <Link to="#" className="hover:text-gray-500 transition-colors">
-                Men's top Wear{" "}
+                Women's bottom Wear{" "}
               </Link>
             </li>
           </ul>
         </div>
+
+        <div>
+          <h3 className="text-lg text-gray-800 mb-4">Support</h3>
+          <ul className="space-y-2 text-gray-600 flex flex-col">
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Contact us{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                About us{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                FAQs{" "}
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Features{" "}
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3></h3>
+          <div className="flex items-center space-x-4">
+            <a
+              href="https:www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-500"
+            >
+              <TbBrandMeta className="h-5 w-5" />
+            </a>
+
+            <a
+              href="https:www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-500"
+            >
+              <IoLogoInstagram className="h-5 w-5" />
+            </a>
+
+            <a
+              href="https:www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-500"
+            >
+              <RiTwitterXLine className="h-5 w-5" />
+            </a>
+          </div>
+          <p className="text-gray-500">Call Us</p>
+          <p>
+            <FiPhoneCall className=" inline-block mr-2
+            " />
+            123-456789
+          </p>
+        </div>
+      </div>
+      <div className="container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-300 pt-6">
+        <p className="text-gray-500 text-sm tracking-tighter text-center">© 2025, compileTab. All Rights Reserved.</p>
       </div>
     </footer>
   );
